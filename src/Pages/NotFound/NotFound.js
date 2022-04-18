024d4ca27049c7f8534bc16a554bb1a3e3ae644e
AkasHZamaN <akashzaman4@gmail.com>
2022-04-18 23:07:37 +0600

    not found area added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Opps !! This file is not avaiable right now</h1>
-            <h1>404</h1>
-        </div>
+        <section>
+            <article className='w-100 text-center'>
+            <img className='w-100 mx-auto' src="https://i.postimg.cc/Dwt1cN2C/404not-Found.png" alt="" />
+            <p >
+            <Link className='text-decoration-none bg-success text-white p-2 rounded-3 fw-bold' to="/">Go Home Page</Link>
+            </p>
+            </article>
+        </section>
     );
 };
 
