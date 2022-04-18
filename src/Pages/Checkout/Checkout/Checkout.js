55c18350a39dc0397061be6f8b93be837958a74a
AkasHZamaN <akashzaman4@gmail.com>
2022-04-18 16:29:11 +0600

    toast added in protective route

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Toast } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Checkout = () => {
-    return (
-        <div>
-            <h1>Thanks for Choosing This Healthy Environment</h1>
-        </div>
-    );
+  return (
+    <div className="w-100 mx-auto">
+      <Toast className="w-50 mx-auto my-5">
+        <Toast.Header>
+          <img style={{height:'90px'}} src="https://i.postimg.cc/d1vXyrwS/alex-gym-stone.png" className="rounded me-2" alt="" />
+          <strong className="me-auto text-success fw-bold">Alex GYM <span className="text-warning">Stone</span></strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body className="text-center text-success fw-bold">Thanks for Hire Me!!</Toast.Body>
+      </Toast>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Checkout;
